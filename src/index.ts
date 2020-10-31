cc68e6a58e6a51bbc40ff14e914921f62cdcc40a
Albin Médoc <stamline203@gmail.com>
2020-10-31 17:13:58 +0100

    Don´t start server if required envirement variable is missing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@ import getApp from './app';
 
 const app = getApp();
 
+if (!process.env.DB_URL) {
+    throw Error('Envirement variable "DB_URL" is required');
+}
+if (!process.env.GOOGLE_MAPS_API_KEY) {
+    throw Error('Envirement variable "GOOGLE_MAPS_API_KEY" is required');
+}
+if (!process.env.BUDGETSMS_USERID) {
+    throw Error('Envirement variable "BUDGETSMS_USERID" is required');
+}
+if (!process.env.BUDGETSMS_USERNAME) {
+    throw Error('Envirement variable "BUDGETSMS_USERNAME" is required');
+}
+if (!process.env.BUDGETSMS_HANDLE) {
+    throw Error('Envirement variable "BUDGETSMS_HANDLE" is required');
+}
+if (!process.env.BUDGETSMS_FROM) {
+    throw Error('Envirement variable "BUDGETSMS_FROM" is required');
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
