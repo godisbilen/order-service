63a69c60e91748fc828ba21bba06cfe716c71d4a
albinmedoc <stamline203@gmail.com>
2021-04-29 14:14:46 +0200

    Delete route for region

--- src/routes/region/delete.ts
+++ src/routes/region/delete.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import validator from 'validator';
+import Region from '../../models/region';
 
 const router = express.Router();
 
 router.delete('/:region_id', (req, res) => {
-    res.send('Not implemented');
+    if (validator.isMongoId(req.params.region_id)) {
+        return res.status(400).json({
+            status: 'Bad Request',
+            message: `'${req.params.region_id}' is not an valid Mongo ObjectID`,
+        });
+    }
+
+    Region.findByIdAndDelete(req.params.region_id)
+        .exec()
+        .then(() => {
+            return res.json({ message: 'Deleted' });
+        })
+        .catch((err: Error) => {
+            res.json(err);
+        });
 });
 
 export default router;
